74c9e30df8ab4b4ed779b7cd59a09d19dae0c9eb
Marco Cedaro <cedmax@users.noreply.github.com>
2019-11-09 19:09:17 +0000

    first draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Calendar from './Calendar';
+
+const songs = new Array(12);
+
+songs.fill([
+  {
+    src: 'https://www.youtube.com/watch?v=r_psWif7vno',
+    name: 'Dark Bells',
+    artist: 'Caracara',
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bk" />
+      <Calendar />
     </div>
   );
 }
