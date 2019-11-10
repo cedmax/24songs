62b77261cd6325398843279d597649d50e0fa473
Marco Cedaro <cedmax@users.noreply.github.com>
2019-11-10 16:25:07 +0000

    first video integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import Modal from 'react-modal';
+import Tuber from 'react-tuber';
 import Calendar from './Calendar';
+import './App.css';
 
 function App({ data, year }) {
+  const [video, setVideo] = useState(null);
+  const close = useCallback(() => {
+    setVideo(null);
+  }, [setVideo]);
   return (
     <div className="App">
       <div className="bk" />
-      <Calendar year={year} data={data} />
+      <Calendar setVideo={setVideo} year={year} data={data} />
+      {video && (
+        <Modal onRequestClose={close} isOpen={!!video}>
+          <Tuber src={video} />
+        </Modal>
+      )}
     </div>
   );
 }
--- src/Calendar.js
+++ src/Calendar.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './Calendar.scss';
-export default ({ year, data }) => (
-  <div className="grid-container">
-    <header className="title">
-      <h1>
-        <span>24</span> songs
-      </h1>
-    </header>
-    {data.map(item => (
-      <div>
-        <img
-          alt={item.artist + ' ' + item.title}
-          src={require(`./data/${year}/${item.img}`)}
-        />
-      </div>
-    ))}
-  </div>
-);
+export default ({ year, data, setVideo }) => {
+  const select = useCallback(
+    e => {
+      const video = e.currentTarget.getAttribute('data-url');
+      setVideo(video);
+    },
+    [setVideo]
+  );
+
+  return (
+    <div className="grid-container">
+      <header className="title">
+        <h1>
+          <span>24</span> songs
+        </h1>
+      </header>
+      {data.map(item => (
+        <button key={item.artist} data-url={item.video} onClick={select}>
+          <img
+            alt={item.artist + ' ' + item.title}
+            src={require(`./data/${year}/${item.img}`)}
+          />
+        </button>
+      ))}
+    </div>
+  );
+};
