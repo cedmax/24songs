428a6cee0f8f5a36de3d41de69b6ddc85e3da378
Marco Cedaro <cedmax@users.noreply.github.com>
2019-11-11 19:47:35 +0000

    fixed embed

--- src/Embed.js
+++ src/Embed.js
@@ -1,10 +1,12 @@
 import React, { memo } from 'react';
 import Tuber from 'react-tuber';
 
-export default memo(({ video }) =>
-  !video.includes('youtube.com') ? (
-    <iframe allow="autoplay" title="embed" src={video} seamless />
-  ) : (
-    <Tuber aspect="16:9" autoplay={true} src={video} />
-  )
+export default memo(
+  ({ video }) =>
+    video &&
+    (!video.includes('youtube.com') ? (
+      <iframe allow="autoplay" title="embed" src={video} seamless />
+    ) : (
+      <Tuber aspect="16:9" autoplay={true} src={video} />
+    ))
 );
