2f0882ec595ca075b87aa6fe4ad18dc24d722a49
Marco Cedaro <cedmax@users.noreply.github.com>
2019-11-12 22:33:43 +0000

    rss

--- functions/rss/rss.js
+++ functions/rss/rss.js
@@ -2,7 +2,7 @@ const data = require("./data.json");
 const now = new Date();
 const year = now.getMonth() === 11 ? now.getFullYear() : now.getFullYear() - 1;
 
-const rss = data => `<?xml version="1.0" encoding="UTF-8"?>
+const rss = (data, total) => `<?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
   <channel>
       <title><![CDATA[24 Songs]]></title>
@@ -21,12 +21,15 @@ const rss = data => `<?xml version="1.0" encoding="UTF-8"?>
         ({ title, artist, img, video }, i) => `<item>
           <title><![CDATA[${title} by ${artist}]]></title>
           <description></description>
-          <link>https://24songs.dsgn.it/${year}/12/${++i}</link>
+          <link>https://24songs.dsgn.it/${year}/12/${total - i}</link>
           <guid isPermaLink="false">${artist}</guid>
           <dc:creator><![CDATA[cedmax]]></dc:creator>
-          <pubDate>${new Date(year, 11, i).toUTCString()}</pubDate>
+          <pubDate>${new Date(year, 11, total - i).toUTCString()}</pubDate>
           <media:content url="https://24songs.dsgn.it/images/${img}" medium="image" />
-          <content:encoded><![CDATA[<iframe width="480" height="270" src="${video}?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>]]></content:encoded>
+          <content:encoded><![CDATA[<iframe width="480" height="270" src="${video.replace(
+            "/watch?v=",
+            "/embed/"
+          )}?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>]]></content:encoded>
       </item>`
       )}
   </channel>
@@ -35,12 +38,16 @@ const rss = data => `<?xml version="1.0" encoding="UTF-8"?>
 
 exports.handler = async (event, context) => {
   const cloneDate = [...data];
-  cloneDate.length = new Date().getDate();
+  if (year === now.getFullYear()) {
+    cloneDate.length = new Date().getDate();
+  } else {
+    cloneDate.length = 24;
+  }
 
   try {
     return {
       statusCode: 200,
-      body: rss(cloneDate)
+      body: rss(cloneDate.reverse(), cloneDate.length)
     };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
