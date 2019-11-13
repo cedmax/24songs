d02af7d83a5a88e66a716817a0615a3022e56522
Marco Cedaro <cedmax@users.noreply.github.com>
2019-11-13 08:10:55 +0000

    enabled deep linking with active video

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,38 @@
-import React, { memo, useState, useCallback } from 'react';
-import Calendar from './Calendar';
-import Embed from './Embed';
-import Modal from './Modal';
+import React, { memo, useState, useCallback } from "react";
+import Calendar from "./Calendar";
+import Embed from "./Embed";
+import Modal from "./Modal";
+
+const date = new Date();
+const urlTokens = window.location.pathname
+  .split("/")
+  .filter(token => !!token)
+  .map(item => parseInt(item, 10));
+
+const isWrongDate = tokens => !tokens[1] || tokens[1] !== 12;
+const isDateNotEnabledYet = tokens =>
+  tokens[0] === date.getFullYear() &&
+  date.getMonth() === 11 &&
+  tokens[2] > date.getDate();
+
+const getPreselected = (year, data, tokens) => {
+  if (!tokens.length) return null;
+  if (isWrongDate(tokens)) {
+    window.location.replace("/");
+    return null;
+  }
+
+  if (isDateNotEnabledYet(tokens)) {
+    window.location.replace("/");
+    return null;
+  }
+
+  const yearIndex = year.findIndex(item => item === tokens[0]);
+  return data[yearIndex][tokens[2] - 1].video;
+};
 
 export default memo(({ data, year }) => {
-  const [video, setVideo] = useState(null);
+  const [video, setVideo] = useState(getPreselected(year, data, urlTokens));
   const close = useCallback(() => setVideo(null), [setVideo]);
   return (
     <>
