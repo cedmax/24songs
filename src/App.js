030eb860d4a17c36118cea0995eb30087d3fb313
Marco Cedaro <cedmax@users.noreply.github.com>
2019-11-21 14:43:58 +0000

    removed mix blend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Footer from "./Footer";
-import Frame from "./Frame";
 import Body from "./Body";
 import "./App.css";
 
 function App({ data, year }) {
   return (
-    <Frame>
+    <>
       <Body data={data} year={year} />
       <Footer />
-    </Frame>
+    </>
   );
 }
 
