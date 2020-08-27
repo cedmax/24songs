43ee5c743ab69d54e4a957ca23cd2d1cd8b33c0f
Marco Cedaro <cedmax@users.noreply.github.com>
2020-08-27 21:30:50 +0100

    Experiment with houdini in the background (#7)
    * experiment with houdini bk
    
    * oops
    
    * small improvements
    
    * fix a quirk on android
    
    * put back react-snap

---
+++ public/cssWorker.js
@@ -0,0 +1,48 @@
+registerPaint(
+  "background",
+  class {
+    static get inputProperties() {
+      return [
+        "--background",
+        "--main",
+        "--accent",
+        "--secondary",
+        "--transparent",
+      ];
+    }
+
+    paint(ctx, geom, props) {
+      const background = props.get("--background").toString();
+      const main = props.get("--main").toString();
+      const accent = props.get("--accent").toString();
+      const secondary = props.get("--secondary").toString();
+      const transparent = props.get("--transparent").toString();
+
+      const rows = [
+        [transparent, transparent, secondary, main],
+        [main, accent, transparent, secondary],
+        [transparent, secondary, main, accent],
+        [secondary, main, accent, transparent],
+      ];
+
+      let { width, height } = geom;
+      const size = 37.5;
+      ctx.fillStyle = background;
+      ctx.fillRect(0, 0, width - 0.5, height - 0.5);
+
+      for (let y = 0; y < height + size; y = y + size) {
+        for (let x = 0; x < width + size; x = x + size) {
+          ctx.beginPath();
+
+          ctx.arc(x, y, size * 0.43, 0, 2 * Math.PI);
+          const row = ((y + size) / size) % rows.length;
+          const col = ((x + size) / size) % rows[row].length;
+          const color = rows[row][col];
+
+          ctx.fillStyle = color;
+          ctx.fill();
+        }
+      }
+    }
+  }
+);
---
+++ src/CSSCustomProperties.js
@@ -0,0 +1,26 @@
+import "css-paint-polyfill";
+
+export const isSupported =
+  "paintWorklet" in window.CSS && "registerProperty" in window.CSS;
+
+export const props = {
+  "--background": "rgb(0, 74, 30)",
+  "--main": "rgb(200, 20, 36)",
+  "--accent": "rgb(0, 95, 28)",
+  "--secondary": "rgb(0, 129, 45)",
+};
+console.log(isSupported);
+export const setup = () => {
+  if (isSupported) {
+    Object.keys(props).forEach(prop => {
+      CSS.registerProperty({
+        name: prop,
+        syntax: "<color>",
+        inherits: false,
+        initialValue: props[prop],
+      });
+    });
+
+    CSS.paintWorklet.addModule("/cssWorker.js");
+  }
+};
